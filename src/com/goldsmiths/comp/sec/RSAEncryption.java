5076691b7f9a3720d4ad8c06c85a7d8e2ee90097
Adam Letch <izzypod5@googlemail.com>
2018-10-29 17:53:07 +0000

    added css/js and modularised into sub folders

---
+++ src/com/goldsmiths/comp/sec/RSAEncryption.java
@@ -0,0 +1,17 @@
+package com.goldsmiths.comp.sec;
+
+import java.math.BigInteger;
+
+public class RSAEncryption {
+
+	public BigInteger encrypt(BigInteger primeMessage) {
+		//1.
+		
+		
+		return primeMessage;
+	}
+
+	public BigInteger dencrypt(BigInteger encryptedNumber) {
+		return encryptedNumber;
+	}
+}
